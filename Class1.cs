6078a1557de082481c3fd3bd953b40a5ccd75580
mituruhiroaki <sekimituru@icloud.com>
2020-09-03 06:22:08 +0900

    対象月の第〇〇曜日の日にちを取得します。

---
+++ Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace TargetDay{
+    public class Class1 {
+
+        #region<クラスフィールド>
+        private static int Year = DateTime.Now.Year;
+        private static CultureInfo culture = CultureInfo.GetCultureInfo("en-US");
+        #endregion
+
+
+        #region<ファンクション>
+        public static Func<int, int, string, int> targetDay = (month, dainann, youbi) => {
+
+            int MonthDays = DateTime.DaysInMonth(Year, month);
+
+            //var culture = CultureInfo.GetCultureInfo("en-US");
+
+            for(int days = 1, count = 1; days <= MonthDays; days++) {
+                var forDay = new DateTime(Year, month, days);
+                if(forDay.ToString("ddd", culture) == youbi ) {
+                    count++;
+                }
+                if(count == 1) {
+                    continue;
+                } else if (count <= dainann) {
+                    days += 6;
+                    continue;
+                } else {
+                    return days;
+                }
+            }
+
+            return -1;
+
+        };
+
+
+        public static Func<int, int, int, string, IEnumerable<int>> targetDayLIst = (startMonth, countMonth, dainann, youbi) => {
+
+            var list = new List<int>();
+
+            for(int c = 0; c < countMonth; c++, startMonth++) {
+                if(startMonth > 12) { Year++; startMonth = 1; }
+                list.Add(targetDay(startMonth, dainann, youbi));
+            }
+
+            IEnumerable<int> result = list;
+
+            return result;
+        };
+
+        #endregion
+
+    }
+}
